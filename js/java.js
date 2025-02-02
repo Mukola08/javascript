a873b4e57286bdff3f5fa49f8ca7fcf806fef6f7
Mukola08 <163895904+Mukola08@users.noreply.github.com>
2025-02-02 22:28:12 +0200

    git const

---
+++ js/java.js
@@ -0,0 +1,21 @@
+const age = 16;
+console.log(age);
+ 
+const name = "Mukola";
+console.log((name));
+
+const isStudent = true;
+console.log(typeof (isStudent));
+
+const myString = "sport is life";
+console.log(myString);
+
+const myNumber = 19;
+const number = 10;
+const numbers = myNumber + number;
+console.log(numbers);
+
+const myNull = null
+console.log(typeof (myNull));
+
+
