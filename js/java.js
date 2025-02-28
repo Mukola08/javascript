69bb43c94341e0c76060cf41d90f93ff8d694318
Mukola08 <163895904+Mukola08@users.noreply.github.com>
2025-02-28 13:54:33 +0200

    pull

--- js/java.js
+++ js/java.js
@@ -1,26 +1,26 @@
-const age = 16;
-console.log(age);
+// const age = 16;
+// console.log(age);
  
-const name = "Mukola";
-console.log((name));
+// const name = "Mukola";
+// console.log((name));
 
-const isStudent = true;
-console.log(typeof (isStudent));
+// const isStudent = true;
+// console.log(typeof (isStudent));
 
-const myString = "sport is life";
-console.log(myString);
+// const myString = "sport is life";
+// console.log(myString);
 
-const myNumber = 19;
-const number = 10;
-const numbers = myNumber + number;
-console.log(numbers);
+// const myNumber = 19;
+// const number = 10;
+// const numbers = myNumber + number;
+// console.log(numbers);
 
-const myNull = null
-console.log(typeof (myNull));
+// const myNull = null
+// console.log(typeof (myNull));
 
-prompt("Введіть ім'я користувача")
-const a = alert("дякую");
-console.log(a);
+// prompt("Введіть ім'я користувача")
+// const a = alert("дякую");
+// console.log(a);
 
 // confirm("Підтвердіть будь-ласка");
 // const a = alert("дякую");
