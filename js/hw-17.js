560b703b68630f0ac3668e07c1b3eaba03b0a637
Mukola08 <163895904+Mukola08@users.noreply.github.com>
2025-05-31 21:39:13 +0300

    hw-17full

--- js/hw-17.js
+++ js/hw-17.js
@@ -13,3 +13,16 @@ textEl.textContent = "Спорт"
 imgEl.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc7UPx7Bt_BslUzwhUcGUS0h6KCwb0fyfrJQ&s";
 imgEl.width = '600'
 
+// 3. Створити розмітку з посиланням та зображенням. За допомогою JavaScript отримати доступ до посилання та змінити значення атрибута "href" на нову URL-адресу. Також отримати доступ до зображення та додати новий атрибут "alt" з описом зображення.
+const linkEl = document.querySelector("a");
+const photoEl = document.querySelector(".photo");
+linkEl.textContent = "facebook"
+linkEl.href = "https://www.facebook.com/?locale=uk_UA";
+photoEl.alt = "Voleyball"
+console.log(photoEl);
+
+// 4. Створити розмітку зі списком елементів. За допомогою JavaScript отримати доступ до першого елемента списку та змінити його вміст на новий текст.
+
+const firstEl = document.querySelector("ul li p")
+firstEl.textContent = firstEl.textContent.replace("text 1", "sportVoleyball")
+
